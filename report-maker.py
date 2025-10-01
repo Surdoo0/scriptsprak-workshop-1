d7d08e3a49104e40bf3327fcd76bca7962614fc3
Davido89 <davnunriv89@gmail.com>
2025-10-01 11:10:57 +0200

    Ändrade print till report

--- report-maker.py
+++ report-maker.py
@@ -4,13 +4,19 @@ import json
 # Open network_devices.json with UTF-8 encoding
 data = json.load(open("network_devices.json", "r",encoding = "utf-8")) 
 
-print("\n===============================\n", "Nätverkstapport - TechCorp AB", "\n===============================\n")
+# Create a variable that holds our whole text report
+report = ""
+report += "\n.............................\n" "Nätverksrapport - TechCorp AB" + "\n.............................\n"
 
 # For each element in the location list
 for location in data["locations"]:
     # Print the name of the location
 
-    print("\n=================\n", location["site"], "\n=================\n")
+    report += "\n" + location["site"] + "\n...............\n"
     # print a list of the host names of the devices on the location
     for device in location["devices"]:
-        print(" ",device["hostname"])
+        report += " " + device["hostname"] + "\n"
+
+# write the report to text file
+with open('report.txt', 'w', encoding='utf-8') as f:
+    f.write(report)
